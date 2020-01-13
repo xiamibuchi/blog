2b4189b240962ef0b0d2ff280a9917b1f5889020
shenyang <772413326@qq.com>
2020-01-13 20:40:10 +0800

    docs:css功能

--- docs/js/utils/download.js
+++ docs/js/utils/download.js
@@ -1,3 +1,51 @@
+function image2base64(img) {
+  let canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+  const mime = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();
+  const dataUrl = canvas.toDataURL("image/" + mime);
+  return dataUrl;
+}
+
+function downloadByAnchor(url, name) {
+  let anchor = document.createElement("a");
+  anchor.href = url;
+  anchor.download = name;
+  anchor.click();
+  return anchor;
+}
+
+function download(value, name, type) {
+  let image = new Image();
+  let objectURL = null;
+  switch (type) {
+    case "img":
+      image.setAttribute("crossOrigin", "Anonymous");
+      image.src = value + "?" + new Date().getTime();
+      image.onload = function() {
+        const imageDataUrl = image2base64(image);
+        downloadByAnchor(imageDataUrl, name);
+      };
+      break;
+    case "binary":
+      if (!("download" in document.createElement("a"))) {
+        // IE10+下载
+        return navigator.msSaveBlob(value, name);
+      }
+      objectURL = window.URL.createObjectURL(new Blob([value]));
+      downloadByAnchor(objectURL, name);
+      URL.revokeObjectURL(objectURL);
+      break;
+    case "file":
+      downloadByAnchor(value, name);
+      break;
+    default:
+      window.open(value);
+  }
+}
+
 // 下载csv
 function handleDownload() {
   let values = [
